26aa6541fc087125085bfe911da21ba7b3cfdf25
Konstantin <konstantinm1006@gmail.com>
2023-09-20 20:36:37 +0300

    second commit

--- js/script2.js
+++ js/script2.js
@@ -1,35 +1,11 @@
-// const names = ["Ivan", "Ann", "ksenia", "Voldemar"];
+function myModule() {
+  this.hello = function () {
+    console.log("Hello");
+  };
 
-// const shortNames = names.filter(function (name) {
-//   return name.length < 4;
-// });
+  this.goodbye = function () {
+    console.log("bye");
+  };
+}
 
-// console.log(shortNames);
-
-// const answers = ["IVAN", "AnnA", "Hello"];
-
-// const result = answers.map((item) => item.toLowerCase());
-
-// console.log(result);
-
-// const some = [4, 6, 10, 15];
-
-// console.log(some.every((item) => typeof item === "number"));
-// const arr = [3, 4, 6, 7, 13, 19];
-
-// const res = arr.reduce((sum, current) => sum - current, 100);
-
-// console.log(res);
-
-const obj = {
-  ivan: "persone",
-  ann: "persone",
-  dog: "animal",
-  cat: "animal",
-};
-
-const newArr = Object.entries(obj)
-  .filter((item) => item[1] === "persone")
-  .map((item) => item[0]);
-
-console.log(newArr);
+module.exports = myModule;
