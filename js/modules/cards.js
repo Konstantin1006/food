d68e97250a32a2b90699b3ccc5381189948b37f0
Konstantin <konstantinm1006@gmail.com>
2023-09-26 18:16:19 +0300

    third commit

--- js/modules/cards.js
+++ js/modules/cards.js
@@ -1,3 +1,5 @@
+import { getResourсe } from "../services/services";
+
 function cards() {
   class MenuCard {
     constructor(src, alt, title, discr, price, parentSelector, ...classes) {
@@ -33,16 +35,6 @@ function cards() {
       this.parent.append(element);
     }
   }
-
-  const getResourсe = async (url) => {
-    const res = await fetch(url);
-
-    if (!res.ok) {
-      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-    }
-
-    return await res.json();
-  };
 
   // getResourсe(" http://localhost:3000/menu").then((data) => {
   //   data.forEach(({ img, altimg, title, descr, price }) => {
@@ -96,4 +88,4 @@ function cards() {
   }
 }
 
-module.exports = cards;
+export default cards;
--- js/modules/timer.js
+++ js/modules/timer.js
@@ -1,6 +1,4 @@
-function timer() {
-  const deadline = "2023-10-01";
-
+function timer(id, deadline) {
   function getTimeRemaining(endtime) {
     let days, hours, minutes, seconds;
     const t = Date.parse(endtime) - Date.parse(new Date());
@@ -58,7 +56,7 @@ function timer() {
     }
   }
 
-  setClock(".timer", deadline);
+  setClock(id, deadline);
 }
 
-module.exports = timer;
+export default timer;
